a75b85c4c70d1e8dae25293353e3f6677ee50cab
Henrique <henrique.sagara@hotmail.com>
2024-11-26 11:32:54 -0500

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Henrique Sagara on 2024-11-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StockAppTests/StockAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppTests.swift
+//  StockAppTests
+//
+//  Created by Henrique Sagara on 2024-11-26.
+//
+
+import Testing
+@testable import StockApp
+
+struct StockAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
