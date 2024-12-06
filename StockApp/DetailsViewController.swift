96b165b77181ff9f198ddb33cfffc1f7fd7f17bd
Henrique <henrique.sagara@hotmail.com>
2024-12-06 16:50:06 -0500

    Details UI

---
+++ StockApp/DetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.swift
+//  StockApp
+//
+//  Created by Henrique Sagara on 2024-12-06.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var roaLabel: UILabel!
+    @IBOutlet weak var roeLabel: UILabel!
+    @IBOutlet weak var dyLabel: UILabel!
+    @IBOutlet weak var peLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var companySymbolLabel: UILabel!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
